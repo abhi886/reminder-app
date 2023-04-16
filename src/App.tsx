9f8090d671cc2402a9f88c5edbdb3e8c87460d61
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2023-04-16 11:30:32 +1000

    Using the state hook and using generic type for state

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import ReminderList from "./components/ReminderList";
 import Reminder from "./models/reminder";
 
-const reminders: Reminder[] = [
-  {
-    id: 1,
-    title: "first",
-  },
-  {
-    id: 2,
-    title: "second",
-  },
-];
 function App() {
+  const [reminders] = useState<Reminder[]>([
+    {
+      id: 1,
+      title: "first",
+    },
+    {
+      id: 2,
+      title: "second",
+    },
+  ]);
   return (
     <div className='App'>
       <ReminderList items={reminders} />
