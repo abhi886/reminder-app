242a3453219b5cf63497afcae1cf1b73eac80c82
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2023-04-22 11:53:25 +1000

    Design Fix: Change column size for the component

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ function App() {
   return (
     <div className='container'>
       <div className='row'>
-        <div className='col-sm'>
+        <div className='col-sm-4'>
           <Reminder></Reminder>{" "}
         </div>
-        <div className='col-sm'>
+        <div className='col-sm-8'>
           <Outlet />
         </div>
       </div>
